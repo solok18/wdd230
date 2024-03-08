4767498221effd636a10afccff31e061e433787f
solok18 <ksolorzano862@gmail.com>
2024-03-07 23:33:02 -0800

    js edits for learning activities

--- scripts/links.js
+++ scripts/links.js
@@ -1,26 +1,45 @@
 const baseURL = "https://solok18.github.io/wdd230/";
-const linkURL = "data/links.json";
+const linkURL = "https://raw.githubusercontent.com/solok18/wdd230/main/data/links.json";
 
 const cards = document.querySelector('#cards');
 
-async function getLinks()   {
+async function getLinks() {
     const response = await fetch(linkURL);
     const data = await response.json();
     // console.log(data);
-    displayLinks(data);
+    displayLinks(data.lessons);
 }
 
 getLinks();
 
-const displayLinks = (weeks) => {
-    weeks.forEach((weeks) => {
-        let list = document.createAttribute('li')
-        let a = document.createAttribute('a')
+const displayLinks = (lessons) => {
+    lessons.forEach((lesson) => {
+        let lessonNumber = document.createElement('h4');
+        let list = document.createElement('ul');
+        // let listItems = document.createElement('li');
+        // let links = document.createElement('a');
 
-        a.textContent = `${weeks.title}`
+        lessonNumber.textContent = `${lesson.lesson}:`;
 
-        list.appendChild(a);
+        lesson.links.forEach((link) => {
+            let listItems = document.createElement('li');
+            let links = document.createElement('a');
+            
+            // listItems.textContent = `${link.title}`;
+            links.href = link.url;
+            links.target = "_blank";
+            links.textContent = link.title ;
 
+        // list.appendChild(lessonNumber);
+        // list.appendChild(listItems);
+        // list.appendChild(links);
+            listItems.appendChild(links);
+            list.appendChild(listItems);
+    });
+        cards.appendChild(lessonNumber);
         cards.appendChild(list);
+        // cards.appendChild(lessonNumber);
     });
 }
+
+
