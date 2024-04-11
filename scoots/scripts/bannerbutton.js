fdb4caf72f73d99322fad501703d4f2d6b40e5f0
solok18 <ksolorzano862@gmail.com>
2024-04-10 21:05:15 -0700

    finalizing edits for scoots page

--- scoots/scripts/bannerbutton.js
+++ scoots/scripts/bannerbutton.js
@@ -6,7 +6,7 @@ document.querySelector(".banner-exit").addEventListener("click", function() {
     this.closest(".banner").style.display = "none";
 });
 
-if (today >= 1 && today <= 3) {
-    document.getElementById("banner").style.display = "block";
-};
+// if (today >= 1 && today <= 3) {
+//     document.getElementById("banner").style.display = "block";
+// };
 
